ffa3693fdf08d6eb7436118568c9d850474c675c
joniumGit <52005121+joniumGit@users.noreply.github.com>
2022-03-14 15:41:19 +0200

    Cleanup code

--- python-client/client.py
+++ python-client/client.py
@@ -3,58 +3,99 @@ import time
 import headers
 import requests
 
-field = [[None for _ in range(0, 8)] for _ in range(0, 8)]
+field = [[]]
 
-r = requests.post('http://localhost/start')
-assert r.status_code == 201, r
-node_start = r.headers[headers.LOCATION]
-auth = r.headers[headers.AUTHORIZATION]
-print('Got Node')
 
-with requests.Session() as client:
-    client.headers[headers.AUTHORIZATION] = auth
+def get_node():
+    r = requests.post('http://localhost/start')
+    assert r.status_code == 201, r
+    print('Got Node')
     time.sleep(2)
     print('Starting')
-    r = client.post(node_start, params=dict(width=8, height=8, mines=10))
-    assert r.status_code == 201, r
-    node = r.headers[headers.LOCATION]
-    print('Field Done')
+    return r.headers[headers.LOCATION], r.headers[headers.AUTHORIZATION]
 
 
-    def print_field():
-        print('  ' + ' '.join(f'{i}' for i in range(0, len(field))))
-        for idx, row in enumerate(field):
-            print(f'{idx}|' + ' '.join(' ' if v is None else f'{v}' for v in row))
+def init_field(node_start) -> str:
+    global field
+    while True:
+        try:
+            width = int(input('Width: '))
+            height = int(input('Height: '))
+            mines = int(input('Mines: '))
+            r = client.post(node_start, params=dict(width=width, height=height, mines=mines))
+            assert r.status_code == 201, r
+            field = [[None for _ in range(0, width)] for _ in range(0, height)]
+            print('Field Done')
+            return r.headers[headers.LOCATION]
+        except (ValueError, AssertionError):
+            print('Failed...')
 
 
+def print_field():
+    print('  ' + ' '.join(f'{i}' for i in range(0, len(field[0]))))
+    for idx, row in enumerate(field):
+        print(f'{idx}|' + ' '.join(' ' if v is None else f'{v}' for v in row))
+
+
+def get_input():
+    while True:
+        input_ = input('Give (f,)x,y: ')
+        parts = input_.split(',')
+        try:
+            if len(parts) == 3:
+                f, x, y = parts
+                x = int(x)
+                y = int(y)
+                f = True
+            else:
+                x, y = parts
+                x = int(x)
+                y = int(y)
+                f = False
+            break
+        except:
+            print('Invalid Input')
+    return f, x, y
+
+
+def handle_flag(x, y):
+    if field[y][x] == 'f':
+        client.delete(f'{node}flag', params=dict(x=x, y=y))
+        field[y][x] = None
+    elif field[y][x] is None:
+        client.post(f'{node}flag', params=dict(x=x, y=y))
+        field[y][x] = 'f'
+
+
+def handle_open(x, y) -> bool:
+    r = client.post(f'{node}open', params=dict(x=x, y=y))
+    if r.status_code == 200:
+        for s in r.json()['items']:
+            if s.get('flag', False):
+                value = 'f'
+            elif s.get('mine', False):
+                if s.get('open', False):
+                    value = 'x'
+                else:
+                    value = '*'
+            else:
+                value = s.get('value', None)
+            field[s['y']][s['x']] = value
+        if r.json().get('status', 'ongoing') != 'ongoing':
+            print_field()
+            print(r.json()['status'])
+            return True
+    return False
+
+
+LOCATION, AUTH = get_node()
+with requests.Session() as client:
+    client.headers[headers.AUTHORIZATION] = AUTH
+    node = init_field(LOCATION)
     while True:
         print_field()
-        x_y = input('Give (f)x,y: ')
-        x, y = x_y.split(',')
-        if 'f' in x:
-            x = x[1]
-            if field[int(y)][int(x)] == 'f':
-                r = client.delete(f'{node}flag', params=dict(x=x, y=y))
-                field[int(y)][int(x)] = None
-            elif field[int(y)][int(x)] is None:
-                r = client.post(f'{node}flag', params=dict(x=x, y=y))
-                field[int(y)][int(x)] = 'f'
-        else:
-            r = client.post(f'{node}open', params=dict(x=x, y=y))
-            if r.status_code == 200:
-                for s in r.json()['items']:
-                    value = None
-                    if s.get('flag', False):
-                        value = 'f'
-                    elif s.get('mine', False):
-                        if s.get('open', False):
-                            value = 'x'
-                        else:
-                            value = '*'
-                    else:
-                        value = s.get('value', None)
-                    field[s['y']][s['x']] = value
-                if r.json().get('status', 'ongoing') != 'ongoing':
-                    print_field()
-                    print(r.json()['status'])
-                    break
+        f_, x_, y_ = get_input()
+        if f_:
+            handle_flag(x_, y_)
+        elif handle_open(x_, y_):
+            break
